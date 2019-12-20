cfff1b63efe3dc44503a281ae261a9c5cb54edf7
zorak070 <s.sucil1234@gmail.com>
2019-12-20 15:40:42 +0545

    save to state for modal fields

--- src/components/Form/form.js
+++ src/components/Form/form.js
@@ -4,124 +4,129 @@ import * as data from "../../JSONfiles/selection.json";
 import * as modalData from "../../JSONfiles/modal.json";
 import Input from '../Input/input';
 import Modal from '../../containers/Modal/modal';
-import {Image,Header,Grid} from "semantic-ui-react";
+import { Image, Header, Grid } from "semantic-ui-react";
 import "./form.css";
 
 class form extends Component {
-  state={
-    formData:{},
-    modalIdentity:"Circular Hollow",
-    modalopen:false,
-    imageData:[],
-    modalData:{}
-  }
-  
-  componentDidMount(){
-    this.setState({formData:elements.default,modalData:modalData.default,imageData:data.default.Loading_Selection});
+  state = {
+    formData: {},
+    modalIdentity: "Circular Hollow",
+    modalopen: false,
+    imageData: [],
+    modalData: {}
   }
 
-  modalclose=()=>{
-    this.setState({modalopen:false , modalIdentity:""});
+  componentDidMount() {
+    this.setState({ formData: elements.default, modalData: modalData.default, imageData: data.default.Loading_Selection });
   }
 
-//   checkValidity(value, rules) {
-//     let isValid = true;
-//     if (rules.required) {
-//         isValid = value.trim() !== '' && isValid;
-//     }
-//     return isValid;
-// }
- 
-modalInputChangeHandler=(event,inputIdentifier,modalIdentity)=>{
-  const updatedmodalData = {
-    ...this.state.modalData
-  };
-  console.log(updatedmodalData);
-  const updatedmodalElement = { 
-    ...updatedmodalData[inputIdentifier]
-  };
-  // console.log(updatedmodalElement)
-  updatedmodalElement.value = event.target.value;
-  // updatedmodalData[inputIdentifier] = updatedmodalElement;
-  // console.log(updatedmodalElement.value);
-  // console.log(updatedmodalData);
-  // this.setState({modalData:updatedmodalData},()=>{
-  //   console.log(this.state.modalData);
-  // });
-}
-  inputChangeHandler = (event,inputIdentifier,modalrender)=>{
+  modalclose = () => {
+    this.setState({ modalopen: false, modalIdentity: "" });
+  }
+
+  //   checkValidity(value, rules) {
+  //     let isValid = true;
+  //     if (rules.required) {
+  //         isValid = value.trim() !== '' && isValid;
+  //     }
+  //     return isValid;
+  // }
+
+  modalInputChangeHandler = (event, inputIdentifier, modalIdentity) => {
+    let updatedmodalData = {
+      ...this.state.modalData
+    };
+    const modalElement = {
+      ...updatedmodalData[modalIdentity]
+    }
+    const updatedmodalElement = {
+      ...modalElement[inputIdentifier]
+    };
+    updatedmodalElement.value = event.target.value;
+    modalElement[inputIdentifier] = updatedmodalElement;
+    console.log(modalElement);
+    updatedmodalData = {
+      ...this.state.modalData,
+      [modalIdentity] : modalElement
+    }
+    console.log(updatedmodalData);
+    this.setState({modalData:updatedmodalData},()=>{
+      // console.log(this.state.modalData);
+    });
+  }
+  inputChangeHandler = (event, inputIdentifier, modalrender) => {
     const updatedFormData = {
       ...this.state.formData
     };
-    const updatedFormElement = { 
+    const updatedFormElement = {
       ...updatedFormData[inputIdentifier]
     };
     updatedFormElement.value = event.target.value;
     updatedFormData[inputIdentifier] = updatedFormElement;
     // updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
     // console.log(updatedFormElement.valid);
-    
-    this.setState({formData:updatedFormData});
-    if(modalrender===true){
-      this.setState({modalIdentity:event.target.value,modalopen:true});
+
+    this.setState({ formData: updatedFormData });
+    if (modalrender === true) {
+      this.setState({ modalIdentity: event.target.value, modalopen: true });
     }
   }
 
-  onclick=(event,inputIdentifier,modalrender)=>{
-    if(modalrender===true){
-      this.setState({modalIdentity:inputIdentifier,modalopen:true});
+  onclick = (event, inputIdentifier, modalrender) => {
+    if (modalrender === true) {
+      this.setState({ modalIdentity: inputIdentifier, modalopen: true });
     }
   }
-    render(){
-      const formElementsArray = [];
-        for (let key in this.state.formData) {
-            formElementsArray.push({
-                id: key,
-                config: this.state.formData[key]
-            });
-        }
-    const exceptLoadingform =(
+  render() {
+    const formElementsArray = [];
+    for (let key in this.state.formData) {
+      formElementsArray.push({
+        id: key,
+        config: this.state.formData[key]
+      });
+    }
+    const exceptLoadingform = (
       <div>
-    {formElementsArray.map(formElement => (
-      <Input elementType={formElement.config.elementType}
-             elementConfig={formElement.config.elementConfig}
-             key={formElement.id} label={formElement.config.label}
-             value={formElement.config.value}
+        {formElementsArray.map(formElement => (
+          <Input elementType={formElement.config.elementType}
+            elementConfig={formElement.config.elementConfig}
+            key={formElement.id} label={formElement.config.label}
+            value={formElement.config.value}
             isvalid={!formElement.config.valid}
             shouldValidate={formElement.config.validation}
-             changed={(e)=>this.inputChangeHandler(e,formElement.id,formElement.config.modal)}
-             />          
-    ))}
-    </div>);   
-      const  image=this.state.imageData.map(imageElement => {
-        return(         
+            changed={(e) => this.inputChangeHandler(e, formElement.id, formElement.config.modal)}
+          />
+        ))}
+      </div>);
+    const image = this.state.imageData.map(imageElement => {
+      return (
         <div key={imageElement.name}>
-           <Grid.Column width={4}>
-         {imageElement.name}
-        <Image key={imageElement.name} onClick={(e)=>{this.onclick(e,imageElement.name,imageElement.modal)}} size="medium" src={require(`../../assets/${imageElement.Location}`)} alt={imageElement.name} />
-        </Grid.Column>
+          <Grid.Column width={4}>
+            {imageElement.name}
+            <Image key={imageElement.name} onClick={(e) => { this.onclick(e, imageElement.name, imageElement.modal) }} size="medium" src={require(`../../assets/${imageElement.Location}`)} alt={imageElement.name} />
+          </Grid.Column>
         </div>
-        )
-      })
-       return(
-          <div>
-            <div className="form">
-              { exceptLoadingform }         
-            </div>
-            <Header>Loading Selection</Header>
-            <Grid>
-           <Grid.Row>
+      )
+    })
+    return (
+      <div>
+        <div className="form">
+          {exceptLoadingform}
+        </div>
+        <Header>Loading Selection</Header>
+        <Grid>
+          <Grid.Row>
             {image}
-        </Grid.Row>
+          </Grid.Row>
         </Grid>
-    {this.state.modalopen?<Modal modalIdentity={this.state.modalIdentity}
-    formData={this.state.modalData[this.state.modalIdentity]} 
-     modalopen={this.state.modalopen}
-      modalInputChangeHandler={(e,id,modalIdentity)=>this.modalInputChangeHandler(e,id,modalIdentity)} 
-      modalclose={this.modalclose}/>
-      :null};   
+        {this.state.modalopen ? <Modal modalIdentity={this.state.modalIdentity}
+          formData={this.state.modalData[this.state.modalIdentity]}
+          modalopen={this.state.modalopen}
+          modalInputChangeHandler={(e, id, modalIdentity) => this.modalInputChangeHandler(e, id, modalIdentity)}
+          modalclose={this.modalclose} />
+          : null};
           </div>
-       );
-    }
+    );
+  }
 }
 export default form
--- src/containers/Modal/modal.js
+++ src/containers/Modal/modal.js
@@ -1,50 +1,56 @@
 import React from "react";
-import { Modal, Image, Grid } from 'semantic-ui-react';
+import { Modal, Image, Grid, Button } from 'semantic-ui-react';
 import Input from '../../components/Input/input';
 
-  const modal = ( props ) => {
-    const modalId = props.modalIdentity;
-    const modalArray = [];
-    for (let key in props.formData) {
-      modalArray.push({
-        modalIdentity:props.modalIdentity,
-        id: key,
-        config: props.formData[key],  
-      });
-    }
-    const modalInput = modalArray.map(modalElement => {
-      return modalElement.config.elementType !== "image" ?
-      <Input elementType={modalElement.config.elementType}
-         elementConfig={modalElement.config.elementConfig}
-                  label={modalElement.config.label}  
-                  value={modalElement.config.value}
-                key={modalElement.id}
-                  changed={(e,id,modalidentity)=>props.modalInputChangeHandler(e,modalElement.id,modalId)}
-                  /> :null
+const modal = (props) => {
+  const modalId = props.modalIdentity;
+  const modalArray = [];
+  for (let key in props.formData) {
+    modalArray.push({
+      modalIdentity: props.modalIdentity,
+      id: key,
+      config: props.formData[key],
     });
-    const modalImage = modalArray.map(modalElement => (
-    modalElement.config.Location !== undefined?<Image key={modalElement.id} wrapped size='small' src={require(`../../assets/${modalElement.config.Location}`)}/>:null));
+  }
+  const modalInput = modalArray.map(modalElement => {
+    return modalElement.config.elementType !== "image" ?
+      <Input elementType={modalElement.config.elementType}
+        elementConfig={modalElement.config.elementConfig}
+        label={modalElement.config.label}
+        value={modalElement.config.value}
+        key={modalElement.id}
+        changed={(e, id, modalidentity) => props.modalInputChangeHandler(e, modalElement.id, modalId)}
+      /> : null
+  });
+  const modalImage = modalArray.map(modalElement => (
+    modalElement.config.Location !== undefined ? <Image key={modalElement.id} wrapped size='small' src={require(`../../assets/${modalElement.config.Location}`)} /> : null));
 
-    return (
-      <div>
-        <Modal 
-        style={{marginTop:"-240px"}}
+  return (
+    <div>
+      <Modal
+        style={{ marginTop: "-240px" }}
         centered={false}
-        open={props.modalopen} 
+        open={props.modalopen}
         modalIdentity={props.modalIdentity}
         closeIcon onClose={props.modalclose} >
-     <Modal.Header>{props.modalIdentity}</Modal.Header>
-     <Modal.Content  image>
- <Grid>
-      <Grid.Row>
-        <Grid.Column width={8}>{modalInput}</Grid.Column>
-        <Grid.Column width={8}>{modalImage}</Grid.Column>          
-          </Grid.Row>
+        <Modal.Header>{props.modalIdentity}</Modal.Header>
+        <Modal.Content image>
+          <Grid>
+            <Grid.Row>
+              <Grid.Column width={8}>{modalInput}</Grid.Column>
+              <Grid.Column width={8}>{modalImage}</Grid.Column>
+            </Grid.Row>
           </Grid>
-          </Modal.Content>
-        </Modal>
-      </div>
-    );
-  }
+        </Modal.Content>
+        <Modal.Actions>
+          <Button onClick={props.modalclose}>Cancel</Button>
+          <Button primary onClick={props.modalInputChangeHandler}>
+            Add
+          </Button>
+        </Modal.Actions>
+      </Modal>
+    </div>
+  );
+}
 
-  export default modal;
+export default modal;
