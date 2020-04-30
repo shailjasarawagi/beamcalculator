842283f8b6219bfca16e130846d2bf75e990a881
shailjasarawagi <shailjasarawagi@gmail.com>
2020-04-30 11:15:11 +0545

    oidc added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import React from 'react';
-import BeamForm from '../src/Container/BeamForm/beamform';
+import React, { Component } from 'react';
+// import BeamForm from '../src/Container/BeamForm/beamform';
 import './App.css';
-import Ribbon from '../src/Components/UI/Ribbon/ribbon';
-import Footer from '../src/Components/UI/Footer/footer';
-// import Oidc from '../src/Container/oidc/Oidc';
+// import Ribbon from '../src/Components/UI/Ribbon/ribbon';
+// import Footer from '../src/Components/UI/Footer/footer';
+import Oidc from '../src/Container/oidc/Oidc';
 
-function App() {
-  return (
-    <div className="App">
-      <Ribbon />
+class App extends Component {
+  render() {
+    return (
+      // <div className="App">
+      /* <Ribbon />
       <div style={{ paddingTop: "100px" }}>
         <BeamForm />
       </div>
-      <Footer />
-      {/* <div>
+      <Footer /> */
+      < div >
         <Oidc />
-      </div> */}
-    </div>
-  );
+      </div >
+      // </div>
+    );
+  }
 }
 
+
 export default App;
+
--- src/hoc/Axios-orders.js
+++ src/hoc/Axios-orders.js
@@ -2,8 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
   // baseURL:process.env.REACT_APP_BACKEND_URL;
-  // baseURL: ' http://192.168.0.22:8000/'
-  baseURL: 'http://66fe6081.ngrok.io/'
+  baseURL: 'http://aaab2381.ngrok.io/'
 });
 
 export default instance;
