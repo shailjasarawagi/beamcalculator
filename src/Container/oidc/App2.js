8a006e2d00c8f44a858f8676893e9ffb35d9433f
sazan10 <070bex432@ioe.edu.np>
2020-05-03 22:54:12 +0545

    implemented newer version of oidc

--- src/Container/oidc/App2.js
+++ src/Container/oidc/App2.js
@@ -3,7 +3,7 @@
  */
 import React, { Component } from 'react';
 import classes from './App2.css';
-import axios from '../../hoc/Axios-orders';
+//import axios from '../../hoc/axios-orders';
 import { withRouter } from 'react-router-dom';
 
 /**send_request() is called when user is not authenticated and a new window is opened */
@@ -19,26 +19,29 @@ class App2 extends Component {
   state = {
     redirect_url: null
   }
-
+  
   /**This opens a new window when user not authenticated */
   send_request = () => {
     // console.log("request sent")
-    axios.get('/oidc/auth/request/', {})
-      .then(response => {
-        // console.log("hello response", response.data);
-        // this.setState({
-        //   redirect_url: "df"
-        // })
-        // this.pooling_id = response.data.pooling_id;
-        this.window = window.open(response.data.redirect_url, "_self");
-        // localStorage.setItem("pollingId", this.pooling_id);
-      })
-      .catch((error) => {
-        console.log("error", error);
-      });
+    // axios.get('/oidc/auth/request/', {})
+    //   .then(response => {
+    //     // console.log("hello response", response.data);
+    //     // this.setState({
+    //     //   redirect_url: "df"
+    //     // })
+    //     // this.pooling_id = response.data.pooling_id;
+    //     this.window = window.open(response.data.redirect_url, "_self");
+    //     // localStorage.setItem("pollingId", this.pooling_id);
+    //   })
+    //   .catch((error) => {
+    //     console.log("error", error);
+    //   });
+
+        const url=process.env.REACT_APP_BACKEND_URL;
+      window.open(url+"/oidc/auth/request/", "_self");
   }
 
-
+ 
   render() {
     return (
       <div className={classes.App2}>
